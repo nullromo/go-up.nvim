bfc5f2cdf7169ebd453695699cbdafd8ba00096c
Kyle Kovacs <kkovacs@diconfiberoptics.com>
2025-01-10 09:57:46 -0800

    move setup code to setup file and keep init file small

--- lua/go-up/init.lua
+++ lua/go-up/init.lua
@@ -1,72 +1,25 @@
 local internals = require('go-up.internals')
 local options = require('go-up.options')
+local setup = require('go-up.setup')
 
 local M = {}
 
-local function autocmd_setup()
-    -- every time the text changes, make sure the virtual lines are not in the
-    -- middle of the text
-    local events = { 'BufEnter', 'TextChanged', 'TextChangedI' }
-    vim.api.nvim_create_autocmd(events, {
-        callback = internals.redraw,
-        group = vim.api.nvim_create_augroup('go-up', {}),
-        desc = 'go-up: redraw virtual toplines',
-    })
-end
-
-local function keymaps_setup()
-    -- adjust the scroll result when using zz to center the screen
-    vim.keymap.set('n', 'zz', internals.centerScreen, {
-        desc = 'go-up: center screen',
-    })
-end
-
-local function usercmds_setup()
-    vim.api.nvim_create_user_command('GoUpReset', internals.redraw, {
-        desc = 'Go-Up reset function',
-    })
-
-    vim.api.nvim_create_user_command('GoUpAlignTop', internals.alignTop, {
-        desc = 'Go-Up align top function',
-    })
-
-    vim.api.nvim_create_user_command('GoUpAlignBottom', internals.alignBottom, {
-        desc = 'Go-Up align bottom function',
-    })
-
-    vim.api.nvim_create_user_command('GoUpAlign', internals.align, {
-        desc = 'Go-Up align function',
-    })
-end
-
-local function modifySettings()
-    if not M.opts.respectSplitkeep then
-        vim.opt.splitkeep = 'topline'
-    end
-    if not M.opts.respectScrolloff then
-        vim.opt.scrolloff = 0
-    end
-end
-
 -- main setup function for Go-Up.nvim
 M.setup = function(opts)
+    -- merge default options into user-defined options
     M.opts = vim.tbl_deep_extend('keep', opts or {}, options.defaultOptions)
-
     options.validateOptions(M.opts)
 
-    -- modify settings
-    modifySettings()
-
-    autocmd_setup()
-    if M.opts.mapZZ then
-        keymaps_setup()
-    end
-    usercmds_setup()
+    -- set up the plugin
+    setup.modifySettings()
+    setup.setupAutocommands()
+    setup.setupKeymaps(opts)
+    setup.setupUserCommands()
 end
 
--- public functions
+-- export public functions
 M.centerScreen = internals.centerScreen
-M.reset = internals.redraw
+M.redraw = internals.redraw
 M.alignTop = internals.alignTop
 M.alignBottom = internals.alignBottom
 M.align = internals.align
---
+++ lua/go-up/setup.lua
@@ -0,0 +1,49 @@
+local internals = require('go-up.internals')
+
+local M = {}
+
+M.setupAutocommands = function()
+    -- every time the text changes, make sure the virtual lines are not in the
+    -- middle of the text
+    vim.api.nvim_create_autocmd({ 'TextChanged', 'TextChangedI' }, {
+        callback = internals.redraw,
+        group = vim.api.nvim_create_augroup('go-up', {}),
+        desc = 'Go-Up.nvim: redraw virtual lines',
+    })
+end
+
+M.setupKeymaps = function(opts)
+    if opts.mapZZ then
+        -- adjust the scroll result when using zz to center the screen
+        vim.keymap.set('n', 'zz', internals.centerScreen, {
+            desc = 'Go-Up.nvim: center the screen',
+        })
+    end
+end
+
+M.setupUserCommands = function()
+    -- each top-level function gets its own user command for easy access
+    vim.api.nvim_create_user_command('GoUpRedraw', internals.redraw, {
+        desc = 'Go-Up.nvim: redraw virtual lines',
+    })
+    vim.api.nvim_create_user_command('GoUpAlignTop', internals.alignTop, {
+        desc = 'Go-Up.nvim: scroll to hide top virtual lines',
+    })
+    vim.api.nvim_create_user_command('GoUpAlignBottom', internals.alignBottom, {
+        desc = 'Go-Up.nvim: scroll to hide bottom virtual lines',
+    })
+    vim.api.nvim_create_user_command('GoUpAlign', internals.align, {
+        desc = 'Go-Up.nvim: scroll to hide virtual lines',
+    })
+end
+
+M.modifySettings = function(opts)
+    if not opts.respectSplitkeep then
+        vim.opt.splitkeep = 'topline'
+    end
+    if not opts.respectScrolloff then
+        vim.opt.scrolloff = 0
+    end
+end
+
+return M
